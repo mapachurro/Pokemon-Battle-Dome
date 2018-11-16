9d89c383e1966f906c270d37ce9db07c684a3adc
Oliver Renwick <oliver.renwick@gmail.com>
2018-11-15 23:56:21 -0500

    lotsa ideas, we'll see if I can make them work

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -33,15 +33,20 @@ $(document).ready(function () {
     var defender = "";
     var attacker = "";
     var attackBtn = $("attackButton");
+    //counter for attack rounds and attack power calculation:
+    var count = 0;
+    var defeated = 0;
 
 
 
 
     //Functions to be used:
-
+    function attack(HP, attack) {
+        resultHP = (HP - attack)
+    }
 
     function restartgame() {
-    //As the name implies, this is to restart the game once game has been won (currently under development)
+        //As the name implies, this is to restart the game once game has been won (currently under development)
         $(".fighter_pic").css("visibility: hidden;")
     };
 
@@ -55,17 +60,46 @@ $(document).ready(function () {
         //When the user clicks on the first image, that'll be the user's avatar ("attacker").
         //When the user clicks on the second image, that'll be the first defender ("defender").
         //If the character has been selected, hide it in the avatar area.
-        //If the character has been selected, make it visible in the fighting area with the appropriate colored border.
+        //If the character has been selected, make it visible in the fighting area with the appropriate colored border. For some reason,
+        //this isn't working correctly. They'll show up in the fighting area, with the colored border, but without their HP counters.
         $("#bulba_ava").click(function () {
             if ((attacker === "") && (defender === "")) {
                 attacker = Bulba;
                 $("#bulba_ava").css({ "visibility": "hidden" });
                 $("#bulba_fi").css({ "visibility": "visible", "border-style": "solid", "border-width": "3px", "border-color": "green" });
+                $("#bulbaHP").css({
+                    "visibility": "visible",
+                    "position": "absolute",
+                    "bottom": "20px",
+                    "width": "250px",
+                    "padding": "5px",
+                    "margin-bottom": "0",
+                    "font-weight": "300",
+                    "line-height": "30px",
+                    "color": "#ffffff",
+                    "text-align": "center",
+                    "background": "#4aaaa5",
+                    "border-bottom": "0"
+                });
             }
-            else if ((defender === "") && (attacker !== Bulba)){
+            else if ((defender === "") && (attacker !== Bulba)) {
                 defender = Bulba;
                 $("#bulba_ava").css({ "visibility": "hidden" });
                 $("#bulba_fi").css({ "visibility": "visible", "border-style": "solid", "border-width": "3px", "border-color": "red" });
+                $("#bulbaHP").css({
+                    "visibility": "visible",
+                    "position": "absolute",
+                    "bottom": "20px",
+                    "width": "250px",
+                    "padding": "5px",
+                    "margin-bottom": "0",
+                    "font-weight": "300",
+                    "line-height": "30px",
+                    "color": "#ffffff",
+                    "text-align": "center",
+                    "background": "#4aaaa5",
+                    "border-bottom": "0"
+                });
             }
         });
         console.log(attacker);
@@ -75,11 +109,39 @@ $(document).ready(function () {
                 attacker = Char;
                 $("#char_ava").css({ "visibility": "hidden" });
                 $("#char_fi").css({ "visibility": "visible", "border-style": "solid", "border-width": "3px", "border-color": "green" });
+                $("#charHP").css({
+                    "visibility": "visible",
+                    "position": "absolute",
+                    "bottom": "20px",
+                    "width": "250px",
+                    "padding": "5px",
+                    "margin-bottom": "0",
+                    "font-weight": "300",
+                    "line-height": "30px",
+                    "color": "#ffffff",
+                    "text-align": "center",
+                    "background": "#4aaaa5",
+                    "border-bottom": "0"
+                });
             }
-            else if ((defender === "") && (attacker !== Char)){
+            else if ((defender === "") && (attacker !== Char)) {
                 defender = Char;
                 $("#char_ava").css({ "visibility": "hidden" });
                 $("#char_fi").css({ "visibility": "visible", "border-style": "solid", "border-width": "3px", "border-color": "red" });
+                $("#charHP").css({
+                    "visibility": "visible",
+                    "position": "absolute",
+                    "bottom": "20px",
+                    "width": "250px",
+                    "padding": "5px",
+                    "margin-bottom": "0",
+                    "font-weight": "300",
+                    "line-height": "30px",
+                    "color": "#ffffff",
+                    "text-align": "center",
+                    "background": "#4aaaa5",
+                    "border-bottom": "0"
+                });
             }
         });
         console.log(attacker);
@@ -89,11 +151,39 @@ $(document).ready(function () {
                 attacker = Dratini;
                 $("#dratini_ava").css({ "visibility": "hidden" });
                 $("#dratini_fi").css({ "visibility": "visible", "border-style": "solid", "border-width": "3px", "border-color": "green" });
+                $("#dratiniHP").css({
+                    "visibility": "visible",
+                    "position": "absolute",
+                    "bottom": "20px",
+                    "width": "250px",
+                    "padding": "5px",
+                    "margin-bottom": "0",
+                    "font-weight": "300",
+                    "line-height": "30px",
+                    "color": "#ffffff",
+                    "text-align": "center",
+                    "background": "#4aaaa5",
+                    "border-bottom": "0"
+                });
             }
             else if ((defender === "") && (attacker !== Dratini)) {
                 defender = Dratini;
                 $("#dratini_ava").css({ "visibility": "hidden" });
                 $("#dratini_fi").css({ "visibility": "visible", "border-style": "solid", "border-width": "3px", "border-color": "red" });
+                $("#dratiniHP").css({
+                    "visibility": "visible",
+                    "position": "absolute",
+                    "bottom": "20px",
+                    "width": "250px",
+                    "padding": "5px",
+                    "margin-bottom": "0",
+                    "font-weight": "300",
+                    "line-height": "30px",
+                    "color": "#ffffff",
+                    "text-align": "center",
+                    "background": "#4aaaa5",
+                    "border-bottom": "0"
+                });
             }
         });
         console.log(attacker);
@@ -103,20 +193,48 @@ $(document).ready(function () {
                 attacker = vapo;
                 $("#vapo_ava").css({ "visibility": "hidden" });
                 $("#vapo_fi").css({ "visibility": "visible", "border-style": "solid", "border-width": "3px", "border-color": "green" });
+                $("#vapoHP").css({
+                    "visibility": "visible",
+                    "position": "absolute",
+                    "bottom": "20px",
+                    "width": "250px",
+                    "padding": "5px",
+                    "margin-bottom": "0",
+                    "font-weight": "300",
+                    "line-height": "30px",
+                    "color": "#ffffff",
+                    "text-align": "center",
+                    "background": "#4aaaa5",
+                    "border-bottom": "0"
+                });
             }
             else if ((defender === "") && (attacker !== Vapo)) {
                 defender = vapo;
                 attacker = vapo;
                 $("#vapo_ava").css({ "visibility": "hidden" });
                 $("#vapo_fi").css({ "visibility": "visible", "border-style": "solid", "border-width": "3px", "border-color": "red" });
+                $("#vapo HP").css({
+                    "visibility": "visible",
+                    "position": "absolute",
+                    "bottom": "20px",
+                    "width": "250px",
+                    "padding": "5px",
+                    "margin-bottom": "0",
+                    "font-weight": "300",
+                    "line-height": "30px",
+                    "color": "#ffffff",
+                    "text-align": "center",
+                    "background": "#4aaaa5",
+                    "border-bottom": "0"
+                });
             }
 
-            //Up till now the attack button has been hidden. Make it visible.
-            $("#attackButton").css({ "visibility": "visible" });
-            //Show them stats
 
-            
+
         });
+        //Up till now the attack button has been hidden. Make it visible.
+        $("#attackButton").css({ "visibility": "visible" });
+        //Show them stats
         console.log(attacker);
         console.log(defender);
 
@@ -126,14 +244,59 @@ $(document).ready(function () {
     function attackLoop() {
         //Attack button behavior:
         $("#attackButton").on("click"(function () {
+            //start counting attack rounds for attack power calculation
+            count++;
+            //then on each successive click, the attacker's base attack is multiplied by that number (of clicks).
+            var attackerPower = ((attacker.base_attack) * count);
+            //Make sure we have both an attacker and a defender
             if ((attacker !== "") && (defender !== "")) {
+                //Give the attacker first attack. The behavior is that we attack base attack
+                for (var HP = defender.healthPoints; HP > 0;) (function (HP) {
+                    $("#attackbutton").onclick = attack(HP, attackerPower);
+                })
+                statReset();
+                //then the defender counterattacks
+                for (var HP = attacker.healthPoints; HP > 0;) (function (HP) {
+                    $("#attackbutton").onclick = attack(HP, defender.counter_attack);
+                })
+                statReset();
+                    //When the defender's HP is depleted, make it disappear and clear out the "defender" category   
+                    if (attacker.healthPoints < 0) {
+                        alert("Oh no! Your pokemon fainted! Try again :-( ");
+                        //Run reset loop, or just refresh the page
+                        defender = "";
+                        attacker = "";
+                        statReset();
+                        restartgame();
+                        playGame();
+                    }
+                    if (defender.healthPoints < 0) {
+                        //If the "defeated enemies" counter is 3, then alert the player they've won and offer them a reset button
+                        if (defeated > 2) {
+                            alert("Congratulations! You've won! Ready to play again?");
+                            //Run reset loop, or just refresh the page
+                            defender = "";
+                            attacker = "";
+                            statReset();
+                            restartgame();
+                            playGame();
+                        }
+                        else {
+                         //Provide an alert that you've defeated your enemy, and it's time to click on another.
+                            alert("Congratulations! You defeated " + defender + "!! Choose another opponent to keep battling!");
+                            defender = "";
+                        //Add a number to the "defeated enemies" counter
+                            defeated++;
+                        }
+                    }
 
             }
         })
-    )}
+        )
+    }
 
     function statReset() {
-        //This keeps the 
+        //This keeps the pokemon's stats up to date on the page. Allegedly.
         $("#bulbaHP").text("HP: " + Bulba.healthPoints);
         $("#charHP").text("HP: " + Char.healthPoints);
         $("#dratiniHP").text("HP: " + Dratini.healthPoints);
@@ -142,17 +305,17 @@ $(document).ready(function () {
 
 
     }
-    
+
 
 
 
     //Essential gameplay:
-
+    statReset();
     restartgame();
     playGame();
-    statReset();
 
- 
+
+
 
 
 
